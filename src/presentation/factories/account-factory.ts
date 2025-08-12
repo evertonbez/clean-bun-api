6bd69adfef3365b5065340a14d19d9107a6d1ee3
Everton <evertonfrnds@gmail.com>
2025-08-12 15:07:03 -0300

    feat: integrar provedor de cache Redis na fábrica de casos de uso para encontrar todas as contas

--- src/presentation/factories/account-factory.ts
+++ src/presentation/factories/account-factory.ts
@@ -1,8 +1,11 @@
+import { ResendMailProvider } from "@/infra/mail/resend-mail-provider";
 import { FindAllAccountsUsecase } from "../../application/usecases/account/find-all-accounts.usecase";
 import { PrismaAccountRepository } from "../../infra/database/repositories/prisma_account_repository";
+import { RedisCacheProvider } from "@/infra/cache/redis-cache-provider";
 
 export function factoryFindAllAccountsUsecase() {
   const accountRepository = new PrismaAccountRepository();
+  const cacheProvider = new RedisCacheProvider();
 
-  return new FindAllAccountsUsecase(accountRepository);
+  return new FindAllAccountsUsecase(accountRepository, cacheProvider);
 }
